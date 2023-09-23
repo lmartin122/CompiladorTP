3bad7bafa45577722cab6309f0659cef6adcd5c2
Fernando Cotti <fernandocotti@hotmail.com>
2023-09-23 16:46:42 -0300

    Impresión y getSymbol() agregados.

--- src/Tokenizer/TablaSimbolos.java
+++ src/Tokenizer/TablaSimbolos.java
@@ -1,21 +1,45 @@
 package Tokenizer;
 
 import java.util.Map;
-import java.util.Set;
 import java.util.HashMap;
 
 public class TablaSimbolos {
 
     private static final Map<Integer, Map<String, String>> tablaSimbolos = new HashMap<>();
+    public static final String valorLexema = "lexema";
     private static int identifierNumber = 0;
 
     public static void addSymbol(String new_symbol){
         Map<String, String> values = new HashMap<>();
-        values.put(new_symbol, new_symbol);
+        values.put(valorLexema, new_symbol);
         tablaSimbolos.put(identifierNumber, values);
         identifierNumber += 1;
     }
 
+    public static int getSymbol(String lex){
+        for (Map.Entry<Integer, Map<String, String>> m: tablaSimbolos.entrySet()){
+            String i = m.getValue().get(lex);
+            if (i == lex) {
+                int ref = m.getKey();
+                return ref;
+            }
+        }
+        return -1;
+        
+    }
+
+    public static void printTable(){
+        System.out.println("Tabla de símbolos: ");
+
+        for(Map.Entry<Integer, Map<String,String>> m: tablaSimbolos.entrySet()){
+            Map<String, String> values = m.getValue();
+            System.out.print(m.getKey() + ": ");
+            for (Map.Entry<String, String> v: values.entrySet()) {
+                System.out.print("(" + v.getKey() + ": " + v.getValue() + ") ");
+            }
+            System.out.println();
+        }
+    }
     
 
 }
