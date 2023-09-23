e1c8949d4dd26054673e198395857489add81f06
Fernando Cotti <fernandocotti@hotmail.com>
2023-09-23 15:21:35 -0300

    Agregado el método para agregar un nuevo símbolo a la tabla de símbolos en la clase de tabla de símbolos.

--- src/Tokenizer/TablaSimbolos.java
+++ src/Tokenizer/TablaSimbolos.java
@@ -1,5 +1,21 @@
 package Tokenizer;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
+
 public class TablaSimbolos {
+
+    private static final Map<Integer, Map<String, String>> tablaSimbolos = new HashMap<>();
+    private static int identifierNumber = 0;
+
+    public static void addSymbol(String new_symbol){
+        Map<String, String> values = new HashMap<>();
+        values.put(new_symbol, new_symbol);
+        tablaSimbolos.put(identifierNumber, values);
+        identifierNumber += 1;
+    }
+
+    
 
 }
